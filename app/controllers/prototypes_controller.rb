5aa24079e9a6e35902ac2234acf54bd4a6ea8653
Yoshmong <arietta44@gmail.com>
2021-09-28 23:55:19 +0900

    first commit

---
+++ app/controllers/prototypes_controller.rb
@@ -0,0 +1,63 @@
+class PrototypesController < ApplicationController
+ 
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @prototypes = Prototype.includes(:user).order("created_at ASC")
+  end
+  
+  def new
+    @prototype = Prototype.new
+  end
+ 
+  def create
+    @prototype = Prototype.new(prototypes_params)
+    if @prototype.save
+      redirect_to root_path      
+    else
+      render :new      
+    end
+  end
+
+  def destroy
+    if @prototype.destroy
+       redirect_to root_path
+    else
+       render :edit
+    end
+
+  end
+
+  
+  def edit
+    unless @prototype.user_id == current_user.id
+      redirect_to action: :index
+    end
+  end
+  
+  def update
+    if @prototype.update(prototypes_params)
+       redirect_to prototype_path(@prototype.id)
+    else
+      @prototype = prototype.update(prototypes_params)
+      render :edit
+    end
+  end
+
+  def show
+    @comment= Comment.new
+    @comments = @prototype.comments.includes(:user)
+  end
+  
+
+  private
+
+  def prototypes_params
+    params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
+  end
+end
---
+++ app/models/prototype.rb
@@ -0,0 +1,15 @@
+class Prototype < ApplicationRecord
+
+belongs_to :user
+has_many :comments, dependent: :destroy
+# prototypeテーブルのレコードが削除された場合、関連しているcommentsテーブルのレコードも同時にdestroyメソッドが実行され、一緒に削除
+
+has_one_attached :image
+
+# with_options presence: true do ... end でもまとめられる?
+validates :title, presence: true
+validates :catch_copy, presence: true
+validates :concept, presence: true
+validates :image, presence: true
+
+end
